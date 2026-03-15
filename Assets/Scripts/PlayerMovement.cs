dbcc2ec8a7b56a98cf87634252536348cefa0eeb
Dragos10 <dragosrazvantudose@gmail.com>
2026-03-15 21:07:18 +0200

    Player mvmt smoother but still chopped

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Sprite[] log4dir;
 
     Rigidbody2D rb;
+    float d, td;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -30,19 +31,33 @@ public class PlayerMovement : MonoBehaviour
         transform.position += (Vector3)dir * Time.deltaTime * 4f;
 
         if (xInput == 0)
+            td = yInput == 0 ? td : (yInput == 1 ? 0 : 4);
+        else if (xInput == 1)
+            td = yInput == 0 ? 2 : (yInput == 1 ? 1 : 3);
+        else
+            td = yInput == 0 ? 6 : (yInput == 1 ? 7 : 5);
+
+        if (Mathf.Abs(td - d) <= 3.5f) d += Mathf.Sign(td - d) * Mathf.Min(Time.deltaTime, Mathf.Abs(td - d)) * 20f;
+        else d = ((d - 20f * Mathf.Sign(td - d) * Mathf.Min(Time.deltaTime, 7 - Mathf.Abs(td - d))) + 7) % 7;
+
+        playerSr.sprite = dirSprites[Mathf.RoundToInt(d)];
+
+        /*if (xInput == 0)
             playerSr.sprite = yInput == 1 ? dirSprites[0] : ((yInput == -1) ? dirSprites[2] : playerSr.sprite);
         else if (xInput == 1)
             playerSr.sprite = (yInput == 1) ? dirSprites[4] : (yInput == -1 ? dirSprites[5] : dirSprites[1]);
         else
-            playerSr.sprite = (yInput == 1) ? dirSprites[7] : (yInput == -1 ? dirSprites[6] : dirSprites[3]);
+            playerSr.sprite = (yInput == 1) ? dirSprites[7] : (yInput == -1 ? dirSprites[6] : dirSprites[3]);*/
 
         //Obj
-        if (xInput == yInput && xInput == 0) return;
 
-        objHeld.transform.localPosition = new Vector2(xInput, yInput).normalized;
-        if (xInput != 0 && yInput == 0) objHeld.sprite = log4dir[0];
-        else if(xInput == 0 && yInput != 0) objHeld.sprite = log4dir[1];
-        else if(xInput == yInput) objHeld.sprite = log4dir[3];
-        else  objHeld.sprite = log4dir[2];
+        int q = Mathf.RoundToInt(d);
+
+        float x = (q > 0 && q < 4) ? 1 : (q > 4 ? -1 : 0);
+        float y = (q < 2 || q > 6) ? 1 : (q > 2 && q < 6 ? -1 : 0);
+
+        objHeld.transform.localPosition = new Vector2(x, y).normalized;
+        objHeld.sprite = q % 4 == 0 ? log4dir[1] : (q % 2 == 0 ? log4dir[0] : (q == 1 || q == 5 ? log4dir[3] : log4dir[2]));
+       
     }
 }
