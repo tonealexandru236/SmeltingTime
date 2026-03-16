c35f969a985baff6b13b9b2d445988eeaa5b1705
Theman360 <158336022+tonealexandru236@users.noreply.github.com>
2026-03-16 22:25:00 +0200

    DONE

--- Assets/Scripts/PlayerHand.cs
+++ Assets/Scripts/PlayerHand.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PlayerHand : MonoBehaviour
 {
-    Sprite[] itemInHandSprites = new Sprite[0];
+    public Sprite[] itemInHandSprites = new Sprite[0];
     public GameObject throwable_item;
 
     public string itemInHandID;
@@ -33,6 +33,7 @@ public class PlayerHand : MonoBehaviour
                 if (closestItem != null && itemInHandID == "") {
                     itemInHandSprites = closestItem.itemSprites;
                     itemInHandID = closestItem.PickUpItem();
+                    Debug.Log(itemInHandID);
 
                     return;
                 }
@@ -45,7 +46,7 @@ public class PlayerHand : MonoBehaviour
 
             station.ActivateStation(false, this);
 
-            if(d < 1.25f && (disStation == -1 || d < disStation))
+            if(d < 1.3f && (disStation == -1 || d < disStation))
             {
                 disStation = d;
                 closestStation = station;
@@ -54,13 +55,7 @@ public class PlayerHand : MonoBehaviour
 
         if(closestStation != null) { /// DACA ESTE O STATIE IN RANGE
             if(Input.GetKeyDown(KeyCode.F))
-            {
-                if (closestStation.gameObject.name == "TrashCan")
-                    StartCoroutine(Item_fly_animation(closestStation.transform.position, 0.8f, 1.6f));
-                //else if (closestStation.gameObject.name == "CraftingTable")
-                //    StartCoroutine(Item_fly_animation(closestStation.transform.position, 0.5f, 1f));
                 closestStation.UseStation(this);
-            }
 
             closestStation.ActivateStation(true, this);
         }
@@ -68,34 +63,8 @@ public class PlayerHand : MonoBehaviour
 
     private float item_fly_speed = 1f;
 
-    public IEnumerator Item_fly_animation(Vector3 to_pos, float duration, float arc_height)
-    {
-        throwable_item.GetComponent<SpriteRenderer>().sprite = itemInHandSprites[2];
-        throwable_item.transform.position = gameObject.transform.position;
-        throwable_item.SetActive(true);
+    
 
-        Vector3 start_pos = transform.position;
-
-        float elapsed = 0;
-
-        while (elapsed < duration)
-        {
-            elapsed += Time.deltaTime;
-
-            float t = elapsed / duration;
-
-            Vector3 cur_pos = Vector3.Lerp(start_pos, to_pos, t);
-
-            cur_pos.y += arc_height * t * (1 - t) * 4;
-            cur_pos.z = -1;
-
-            throwable_item.transform.position = cur_pos;
-
-            yield return null;
-        }
-
-        throwable_item.SetActive(false);
-    }
     public void SetOrientationForItemInHand(int q)
     {
         if (itemInHandID == "") return;
--- Assets/Scripts/StationScript.cs
+++ Assets/Scripts/StationScript.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class StationScript : MonoBehaviour
 {
     public string stationTag;
+    public GameObject throwable_item;
 
     [SerializeField] GameObject arrowVisual;
 
@@ -14,7 +16,8 @@ public class StationScript : MonoBehaviour
         arrowVisual.SetActive(closeInRange);
     }
     public void UseStation(PlayerHand ph) {
-        if(stationTag == "trash") {
+        if(stationTag == "trash" && ph.itemInHandID != "") {
+            StartCoroutine(Item_fly_animation(ph.transform.position, 0.75f, 1.1f, ph.itemInHandSprites[1]));
             ph.RemoveItemInHand();
         }
         else if (stationTag == "crafting") {
@@ -25,5 +28,39 @@ public class StationScript : MonoBehaviour
         closeInRange = false;
         if(actv)
             closeInRange = actv;
+    }
+
+
+    public IEnumerator Item_fly_animation(Vector3 from_pos, float duration, float arc_height, Sprite sprite)
+    {
+        GameObject ist = Instantiate(throwable_item);
+
+        ist.GetComponent<SpriteRenderer>().sprite = sprite;
+        ist.transform.position = gameObject.transform.position;
+        ist.SetActive(true);
+
+        arc_height = (transform.position.y - from_pos.y) + arc_height;
+
+        Vector3 start_pos = from_pos;
+
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            float t = elapsed / duration;
+
+            Vector3 cur_pos = Vector3.Lerp(start_pos, gameObject.transform.position, t);
+
+            cur_pos.y += arc_height * t * (1 - t) * 4;
+            cur_pos.z = -1;
+
+            ist.transform.position = cur_pos;
+
+            yield return null;
+        }
+
+        Destroy(ist);
     }
 }
