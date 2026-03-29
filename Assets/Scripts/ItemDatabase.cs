692b06f9a1d779b467a8f3395a7700ae512b4039
Dragos10 <dragosrazvantudose@gmail.com>
2026-03-29 20:54:37 +0300

    Ummm Fixed ItemDB

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDatabase : MonoBehaviour
 {
-    [SerializeField] string[] itemID;
+    List<string> itemID = new List<string>();
     [SerializeField] ItemScript[] itemAsObj;
 
+    private void Start()
+    {
+        itemID.Clear();
+        foreach (ItemScript iscr in itemAsObj)
+        {
+            bool a = false;
+            string nm = "";
+            for(int i = 0; i < iscr.name.Length; i++)
+            {
+                if (iscr.name[i] == '-') a = true;
+                else if (a && iscr.name[i] != ' ') nm += iscr.name[i];
+            }
+            itemID.Add(nm.ToLower());
+        }
+    }
     public ItemScript GetObjById(string id)
     {
-        for (int i = 0; i < itemID.Length; i++)
+        for (int i = 0; i < itemID.Count; i++)
             if (itemID[i] == id)
                 return itemAsObj[i];
         return null;
