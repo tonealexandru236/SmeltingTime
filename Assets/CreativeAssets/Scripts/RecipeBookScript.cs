7f4b15ffe33aa6c5e2281a40e62c30f6d7be6e3c
Dragos10 <dragosrazvantudose@gmail.com>
2026-04-11 19:33:57 +0300

    Added A Better Recipe Book

---
+++ Assets/CreativeAssets/Scripts/RecipeBookScript.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeBookScript : MonoBehaviour
+{
+    [SerializeField] Image mainRecipeImage;
+
+    [Header("Categories")]
+    [SerializeField] GameObject[] allTabs;
+
+    [Header("RecipeSprites")]
+    [SerializeField] Sprite[] pickaxeSprites;
+    [SerializeField] Sprite[] swordSprites;
+    [SerializeField] Sprite[] axeSprites;
+    [SerializeField] Sprite[] chestplateSprites;
+
+    Sprite[] spritesForMainRecipe;
+
+    string currentTab;
+    float t;
+    private void Start()
+    {
+        t = 0;
+    }
+    private void Update()
+    {
+        
+        if(currentTab != "") {
+            t += Time.deltaTime;
+            t %= spritesForMainRecipe.Length;
+
+            mainRecipeImage.sprite = spritesForMainRecipe[(int)t];
+        }
+        else
+        {
+            t = 0;
+        }
+
+
+        foreach(GameObject obj in allTabs)
+        {
+            if(currentTab == obj.name) {
+                obj.transform.localPosition = Vector2.MoveTowards(obj.transform.localPosition, new Vector3(obj.transform.localPosition.x, 20, 0), Time.deltaTime * 200f);
+            }
+            else
+            {
+                obj.transform.localPosition = Vector2.MoveTowards(obj.transform.localPosition, new Vector3(obj.transform.localPosition.x, 0, 0), Time.deltaTime * 200f);
+            }
+        }
+    }
+    public void SelectTab(string n)
+    {
+        currentTab = n;
+
+        if (n == "Pickaxe") spritesForMainRecipe = pickaxeSprites;
+        if (n == "Sword") spritesForMainRecipe = swordSprites;
+        if (n == "Axe") spritesForMainRecipe = axeSprites;
+        if (n == "Chestplate") spritesForMainRecipe = chestplateSprites;
+    }
+}
