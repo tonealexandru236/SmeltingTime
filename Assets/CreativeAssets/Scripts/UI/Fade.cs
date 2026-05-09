1a0b70e34ecbe3366caf01af9cd6a787cb3daef9
Theman360 <158336022+tonealexandru236@users.noreply.github.com>
2026-05-09 12:56:40 +0300

    Added fades

---
+++ Assets/CreativeAssets/Scripts/UI/Fade.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    public static Fade instance;
+    public Image fade_bg;
+
+    void Awake()
+    {
+        /// Singleton thingy again
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.Log("more than 1 fade thingies!!");
+            Destroy(gameObject);
+        }
+    }
+
+    public void FadeOut(float speed)
+    {
+        Animator bg = fade_bg.GetComponent<Animator>();
+
+        bg.SetFloat("Speed", speed);
+        bg.Play("fade", 0, 0);
+    }
+
+    public void FadeIn(float speed)
+    {
+        Animator bg = fade_bg.GetComponent<Animator>();
+
+        bg.SetFloat("Speed", -speed);
+        bg.Play("fade", 0, 1);
+    }
+}
--- Assets/CreativeAssets/Scripts/UI/OpenScene.cs
+++ Assets/CreativeAssets/Scripts/UI/OpenScene.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,16 @@ public class OpenScene : MonoBehaviour
     public void on_click()
     {
         AudioManager.instance.PlaySound("buttonClick");
+
+        StartCoroutine(do_anim());
+    }
+
+    IEnumerator do_anim()
+    {
+        if (Fade.instance)
+            Fade.instance.FadeIn(1);
+        yield return new WaitForSecondsRealtime(1);
+
         SceneManager.LoadScene(scene_name);
     }
 }
